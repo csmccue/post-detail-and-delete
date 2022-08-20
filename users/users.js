30dfded45bb99e05994146dd0996f37c33c349cb
Connor McCue <connor.mccue@comcast.net>
2022-08-19 17:23:15 -0700

    removed console logs

--- users/users.js
+++ users/users.js
@@ -4,11 +4,9 @@ import { getProfiles } from '../fetch-utils.js';
 import { renderUser } from '../render-utils.js';
 
 const userListEl = document.getElementById('user-list');
-console.log(userListEl);
 
 async function loadUsers() {
     const data = await getProfiles();
-    console.log(data);
     if (data) {
         for (let user of data) {
             const userDiv = renderUser(user);
